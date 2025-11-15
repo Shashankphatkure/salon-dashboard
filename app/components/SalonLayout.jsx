931ac5dbd4a2e46bbcfeff4657c1c5786b9000f2
shashankphatkure <shashankphatkure@gmail.com>
2025-11-15 12:27:39 +0530

    updated

--- app/components/SalonLayout.jsx
+++ app/components/SalonLayout.jsx
@@ -11,13 +11,6 @@ const SalonLayout = ({ children, currentPage }) => {
       <main>
         {children}
       </main>
-
-      {/* Footer */}
-      <footer className="bg-white dark:bg-gray-800 mt-20 py-8 border-t">
-        <div className="container mx-auto px-4 text-center text-gray-600 dark:text-gray-400 text-sm">
-          <p>© {new Date().getFullYear()} Hair & Care Unisex Salon. All rights reserved.</p>
-        </div>
-      </footer>
     </div>
   );
 };
