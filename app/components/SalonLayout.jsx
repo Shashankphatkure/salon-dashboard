78c6e6805cf8427437d2556159f146002320ea9e
shashankphatkure <shashankphatkure@gmail.com>
2025-11-15 12:36:14 +0530

    updated

--- app/components/SalonLayout.jsx
+++ app/components/SalonLayout.jsx
@@ -1,17 +1,19 @@
-import Link from 'next/link';
-import Navbar from './Navbar';
+import { SidebarInset, SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
+import { AppSidebar } from './AppSidebar';
 
 const SalonLayout = ({ children, currentPage }) => {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-purple-50 to-pink-100 dark:from-gray-900 dark:to-purple-900">
-      {/* Use shared Navbar component */}
-      <Navbar />
-
-      {/* Main Content */}
-      <main>
-        {children}
-      </main>
-    </div>
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 border-b bg-white dark:bg-gray-800 px-4">
+          <SidebarTrigger />
+        </header>
+        <main className="flex-1 overflow-auto bg-gradient-to-br from-purple-50 to-pink-100 dark:from-gray-900 dark:to-purple-900">
+          {children}
+        </main>
+      </SidebarInset>
+    </SidebarProvider>
   );
 };
 
